38ebc10b821dfdc17287671eef61afc9853c59d2
Naimur Rahman Dorjoy <rahmandurjoy9@gmail.com>
2026-03-30 21:35:08 +0600

    Add navbar

---
+++ src/Components/Navigation/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import shoppingCart from '../../assets/products/shopping-cart.png'
+
+const Navbar = () => {
+    return (
+        
+        <div className='container'>
+            <nav className='flex justify-between py-5'>
+            <a href="#" className='bg-gradient-to-r from-[#9514FA] to-[#4F39F6] bg-clip-text text-transparent text-4xl font-[800]'>DigiTools</a>
+            <ul className='flex gap-x-8 items-center'>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Pricing</a></li>
+                <li><a href="#">Testimonials</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+            <div className='flex gap-x-3 items-center'>
+                <img src={shoppingCart} alt="Cart" />
+                <button>Login</button>
+                <button className='bg-gradient-to-r from-[#9514FA] to-[#4F39F6] px-4 py-3 rounded-3xl text-white font-[600]'>Get Started</button>
+            </div>
+        </nav>
+        </div>
+    );
+};
+
+export default Navbar;
